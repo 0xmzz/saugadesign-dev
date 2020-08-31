a491215eef265b7a3175cca11bf19de50cb00df5
zubair <zubairzia0@gmail.com>
2020-08-31 17:45:49 -0400

    updates to snipcart rollback

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,7 +1,29 @@
 <template>
-  <p>
-    Comming soon! We located at 6973 Lisanne Crt, Mississauga, Ontaio, Canada.
-  </p>
+  <v-card class="mx-auto" max-width="1400">
+    <v-card-title >Welcome to SaugaDesign!</v-card-title>
+    <v-card-text class="text--primary">
+      <p>
+        We are located in Mississauga, Ontaio, Canada and have been creating
+        custom laser cut products since 2016. Our designs are completely
+        customizable and beautify and update the façade of your home.
+      </p>
+
+      <p>
+        Our mission is to provide you with a one stop shop for customizable
+        signs without hassle or high cost. We are passionate about our work and
+        our friendly service and detailed craftsmanship is why our clients keep
+        returning and referring us to their family and friends. We understand
+        that little upgrades can make a substantial difference and our home
+        signs do just that. We hope you enjoy our products as much as we enjoy
+        creating and offering them to you. Let us help you create pride of
+        ownership in your home and put your best foot forward with SaugaDesigns.
+      </p>
+      <h3>
+        Also, if you want to talk just click on the chat bubble on the bottom
+        right!
+      </h3>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
